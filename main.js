ad2eada5382ebf381dbbfe389fd66d7f4904303a
nlangle <nicholas.langle@nrel.gov>
2017-02-24 16:16:38 -0700

    vue test

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-//Vue Test
+//Vue Basic Example
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Tech + Nick + L + Vue!'
+    message: 'Working with Vue!'
   }
 });
+
+//Vue ToDo Example
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+var componentApp = new Vue({
+  el: '#component-app',
+  data: {
+    nicksList: [
+      { text: 'Learn Vue' },
+      { text: 'Style Site' },
+      { text: 'Expand Site with Vue' }
+    ]
+  }
+})
